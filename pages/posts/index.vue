3c58df73d7e003e0d0a13633c470c8bd9f5f9861
Jimena Miguez <jimeblau@gmail.com>
2022-08-31 14:50:27 +0200

    Finish creating the main section pages

---
+++ pages/posts/index.vue
@@ -0,0 +1,22 @@
+<!-- 4) Create the skeleton for the /posts route and
+add desired styles. Later on I will render a list of
+post here. -->
+<!-- 5) Inside the posts folder, I want to have a 
+subpage with a dynamic parameter. The parameter
+should be called id. So, inside the posts folder,
+create the _id folder. Inside the _id folder,
+create an index.vue file, where a single post
+will be shown. Next step in posts > _id > index.vue -->
+<template>
+  <div class="posts-page">
+
+  </div>
+</template>
+
+<style scoped>
+.posts-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
