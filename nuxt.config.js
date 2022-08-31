7116e674c2ab9ffed0a13c44db9e63a8f40ca91f
Jimena Miguez <jimeblau@gmail.com>
2022-08-31 15:23:19 +0200

    Finish adding external fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,8 +13,20 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
+    /* EXPLANATION: I want to add external fonts to my project. I will 
+    use fonts from google fonts */
+    /* 1) Go to google fonts, select the fonts you want and have a look
+    at the link that we should paste normally in the head of a project. */
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      /* 2) In order for my entire project can have the selected fonts,
+      add them in the nuxt.config.js file. Here, under the section links,
+      I will have an array of links I want for my project. The array contains
+      several objects representing each link I want. Here I can add the
+      link for my fonts. In the rel: attribute add 'stylesheet' and in 
+      the href: attribute add the url provided in the link by google fonts.
+      The next steps are in layouts > default.vue */
+      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css?family=Open+Sans" }
     ]
   },
 
