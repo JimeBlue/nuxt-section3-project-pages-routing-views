3db4c0ac73890e898d9ff44df49e1691fffba5ed
Jimena Miguez <jimeblau@gmail.com>
2022-08-31 18:50:45 +0200

    Delete comments

--- components/Posts/PostPreview.vue
+++ components/Posts/PostPreview.vue
@@ -1,30 +1,9 @@
 <template>
-<!-- 3)Add the structure for a post preview component.
-The post preview should be a link going to the individual post page. 
-Therefore, It will be wrapped into the nuxt-link tag.The post preview 
-won't be the complete post, but it'll have a title,an image and a 
-preview text.
--->
-      <!-- 18) Add the id property to the to attribute -->
       <nuxt-link :to="'/posts/' + id" class="post-preview">
-        <!-- 4) To be semantic, The post preview should be 
-        inside the artible tag. Remember that this comoponnet
-        will be added to the main page and it will be
-        repeated there, each time for a different post.
-         All I add inside the article tag becomes a link. -->
         <article>
-          <!-- 5) I added the image by having inline style with
-          the background image url property. -->
-          <!-- 21) Add the thumbnail property. So add data
-         binding to the style attribute (:style). Then 
-         set the value of :style to: "{backgroundImage: 'url(' + thumbnail + ')'}" --> 
           <div class="post-thumbnail" :style="{backgroundImage: 'url(' + thumbnail + ')'}"></div>
-          <!-- 22) Now go to the main page and pass the values of all
-          the properties as props. Next steps in the main page -->
           <div class="post-content">
-            <!-- 19) Add the propery title -->
             <h1>{{ title }}</h1>
-            <!-- 20) Add the propery previewText -->
             <p>{{ previewText }}</p>
           </div>
         </article>
@@ -33,34 +12,20 @@ preview text.
 
 <script>
     export default {
-        /* 12) Add a name to the component. 
-        Very important for debugging! */
-        name: "PostPreview",
-        /* 13) Add the props property, which 
-        will include the props this component
-        works with. */
-        
+        name: "PostPreview", 
         props: {
-            /* 14) Add the id prop and define it by
-        passing an object and setting the type
-        to string as well as setting the required
-        to true. The later is important because we
-        need the id of a post in order to render it. */
             id: {
             type: String,
             required: true,
             },
-            /* 15)Add a title property. */
             title: {
             type: String,
             required: true,
             },
-            /* 16)Add a previewText property. */
             previewText: {
             type: String,
             required: true,
             },
-             /* 17)Add a thumbnail property. */
             thumbnail: {
             type: String,
             required: true,
--- pages/index.vue
+++ pages/index.vue
@@ -3,30 +3,13 @@
     <section class="intro">
       <h1>Get the latest tech news!</h1>
     </section>
-    <!-- EXPLANATION: I want to create a component for post. 
-    Right now, I have a list of posts and I have hardcoded each
-    post in the list. But what I want instead is to have a
-    a component called post and add it to the list. -->
-    <!-- 1) Go to the components folder and create a folder
-    called Posts, where  I will store all my posts related
-    comoponents. -->
-    <!-- 2) Inside the Posts folder create the PostPreview.vue
-    file. Next steps there. -->
-    <!-- 7) Create a section where the PostPreview component
-    will be in and style it. -->
     <section class="featured-posts">
-      <!-- 9) Use the PostPreview component -->
-      <!-- 23) Pass the content of the PostPreview component as
-      props -->
       <PostPreview 
       id="1"
       thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
       title="Hello there"
       previewText="This is my first post!"
       />
-      <!-- 10) Replicate it a few times to have several posts -->
-      <!-- 24) Pass the content of the replicated posts as props
-      as well. -->
       <PostPreview 
       id="2"
       thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
@@ -37,21 +20,14 @@
       thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
       title="Hello number 3"
       previewText="This is my third post!"/>
-      <!-- 11) We need to pass data to the PostPreview component
-      because each PostPreview component displayed here
-      will be different and will have different information/data.
-      For that purpose I'll use props. Next step in 
-      the PostPreview component -->
     </section>
   </div>
 </template>
 
 <script>
-// 8) Import the PostPreview component
 import PostPreview from '@/components/Posts/PostPreview'
 
 export default {
-  // Register the PostPreview component
   components: {
     PostPreview
   }
