c61c87aa0dc35c2a5762ff68ef7131f218418264
Jimena Miguez <jimeblau@gmail.com>
2022-08-31 20:59:37 +0200

    Finish adding the header component

---
+++ components/Navigation/TheHeader.vue
@@ -0,0 +1,101 @@
+<!-- What this component includes: this component includes the
+TheSideNavToggler component and listens to the custom toggle
+event the TheSideNavToggler is emiting (@toggle). It then 
+emits the event sidenavToggle, which we´ll have to handle.
+This component also includes a logo that is a text in a link.
+It also includes a navbar with a list of nuxt-links. -->
+<template>
+<div class="header-container">
+  <header class="the-header">
+    <TheSideNavToggle @toggle="$emit('sidenavToggle')" />
+    <div class="logo">
+      <nuxt-link to="/">WD BLOG</nuxt-link>
+    </div>
+    <div class="spacer"></div>
+    <div class="navigation-items">
+      <ul class="nav-list">
+        <li class="nav-item"><nuxt-link to="/posts">Blog</nuxt-link></li>
+        <li class="nav-item"><nuxt-link to="/about">About</nuxt-link></li>
+        <li class="nav-item"><nuxt-link to="/admin">Admin</nuxt-link></li>
+      </ul>
+    </div>
+  </header>
+</div>
+</template>
+
+<script>
+import TheSideNavToggle from "@/components/Navigation/TheSideNavToggle";
+
+export default {
+  name: "TheHeader",
+  components: {
+    TheSideNavToggle
+  }
+};
+</script>
+
+
+<style scoped>
+.header-container {
+  height: 60px;
+}
+
+.the-header {
+  width: 100%;
+  position: fixed;
+  height: 60px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: black;
+  z-index: 100;
+  box-sizing: border-box;
+  padding: 0 20px;
+}
+
+.logo {
+  margin: 0 10px;
+  font-size: 1.3rem;
+}
+
+.logo a {
+  text-decoration: none;
+  color: white;
+}
+
+.spacer {
+  flex: 1;
+}
+
+.navigation-items {
+  display: none;
+}
+
+@media (min-width: 768px) {
+  .navigation-items {
+    display: block;
+  }
+}
+
+.nav-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+}
+
+.nav-item {
+  margin: 0 10px;
+}
+
+.nav-item a {
+  text-decoration: none;
+  color: white;
+}
+
+.nav-item a:hover,
+.nav-item a:active,
+.nav-item a.nuxt-link-active {
+  color: red;
+}
+</style>
