f43a8fd61791bce97757003b4a85a22b46ea8228
Jimena Miguez <jimeblau@gmail.com>
2022-08-31 15:26:10 +0200

    Delete comments

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,15 +5,9 @@
 </template>
 
 <style>
-  /* 3) Add the font family to the whole html */
 html {
   font-family: 'Open Sans', sans-serif;
 }
-
-/* IMPORTANT: here I can add all the resets I normally add, 
-like marging 0 for the body and the ::after, ::before box-sizing.
-Remember that I a have more than one layout these styles only will
-be applied to the default layout and not the other ones. */
 
 body {
   margin: 0;
