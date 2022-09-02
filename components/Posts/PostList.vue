9605d0c73cdb133e806855061925d11da8723b56
Jimena Miguez <jimeblau@gmail.com>
2022-09-02 12:57:01 +0200

    Finish preparing the backend -create reusable form

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -1,18 +1,25 @@
 <template>
    <section class="post-list">
+    <!-- 15) If isAdmin is true, I need to ajust my links. As the links
+    are in the PostPreview component, I pass the information to it,
+    with data binding (:is-admin = "isAdmin" ). Next sept in 
+    the PostPreview component.-->
       <PostPreview 
       id="1"
+      :is-admin = "isAdmin"
       thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
       title="Hello there"
       previewText="This is my first post!"
       />
       <PostPreview 
       id="2"
+      :is-admin = "isAdmin"
       thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
       title="Hello number 2"
       previewText="This is my second post!"/>
       <PostPreview 
       id="3"
+      :is-admin = "isAdmin"
       thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
       title="Hello number 3"
       previewText="This is my third post!"/>
@@ -23,6 +30,13 @@
     export default {
     components: {
     PostPreview
+  },
+  props: {
+    /* 14) Add props to check whether the user is an admin. */
+    isAdmin: {
+      type: Boolean,
+      dafault: false
+    }
   }
     }
 </script>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,58 @@
+<template>
+    <!-- EXPLANATION: in the admin component I want the
+    user to be able to see a post, click on it and
+    edit it. Therefore, in this component I need to 
+    include the form again. The same form I have in the new-post file.
+    For that purpose, I´ll create the form as a reusable component. -->
+    <div class="admin-post-page">
+        <section class="update-form">
+            <!-- 1) In the folder components create the Adim folder
+        and add the AdminFormPost.vue file. Next step there. -->
+        <!-- 10) Use the AdminFormPost component */-->
+        <!-- 11) The AdminFormPost must accept the loadedPost
+        object, so I use data binding (:post="loadedPost").
+        Next step in AdminFormPost component. -->
+           <AdminFormPost :post="loadedPost"/>
+        </section>
+    </div>
+</template>
+
+<script>
+    /* 8) Import the AdminFormPost component */
+import AdminFormPost from "@/components/Admin/AdminFormPost"
+export default {
+    /* 9) Register the AdminFormPost component */
+    components: {
+        AdminFormPost
+    },
+    data() {
+        return {
+             /* 7) I want the form to include the data the user
+    has already entered so that he/she can edit it. 
+    So, to load the form data, I create the loadedPost 
+    object. Later on, I will dynamically fetch loadedPost from
+    the server.*/
+    /* 8) The loadedPost must be the post I want to edit. I first,
+    fill in the loadedPost object. */
+            loadedPost: {
+                author: "Jimena",
+                title: "My awsome post",
+                content: "Super amazing, thaks for that!",
+                thumbnailLink: "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+            }
+        }
+    },
+}
+</script>
+
+<style scoped>
+    .update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
